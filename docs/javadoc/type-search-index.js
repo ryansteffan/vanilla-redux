541cd06dff1394265ad5a7dfbd988822c94403b1
Ryan Steffan <ryansteffan.biz@gmail.com>
2025-09-03 17:21:54 -0500

    Added the basic java docs to the docs folder.

---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ryansteffan.vanillaredux","l":"Config"},{"p":"com.ryansteffan.vanillaredux","l":"Vanillaredux"}];updateSearchResults();
